78f34964c80662757cb55a3958f694683a1b2585
Cadmia <101380039+cadmia17@users.noreply.github.com>
2022-03-11 04:18:46 +0000

    started repo + started remake of texit module

---
+++ pdf/texit.py
@@ -0,0 +1,20 @@
+import json, shutil
+from urllib.parse import quote as encode
+from pdf_templates import PDF
+
+base = "https://chart.googleapis.com/chart?cht=tx&chl="
+
+def get_formula(formula, output="pdf/_texit.png"):
+  en_formula = encode(formula)
+
+class PDF(PDF):
+  def parse(self, dic):
+    for key in dic.keys():
+      print(key)
+      key_short = key[0] + key[-1]
+
+      k
+
+
+
+get
